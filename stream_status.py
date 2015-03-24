0d3661f8c439476e91baa0800f8e0210ce6cbb19
jekku0966 <Jerva@Jerva-iMac.local>
2015-03-24 23:47:21 +0200

    Added a better looking Slack post, added user adding and removing. Made the script to work with Heroku.

--- stream_status.py
+++ stream_status.py
@@ -1,27 +1,43 @@
 import requests
 import json
-from apscheduler.schedulers.background import BlockingScheduler
+from apscheduler.schedulers.background import BackgroundScheduler
+import MySQLdb
+import cherrypy
 
+job_defaults = {
+    'coalesce': True,
+    'max_instances': 3
+}
 
+# Add a line for a test of changes
 # Posted urls
 online = []
 
+# MySQL database credentials, I advice you to use an external file for this
+cnx = MySQLdb.connect(user='USERNAME', passwd='PASSWORD',
+                              host='IP_ADDRESS',
+                              db='YOU_DATABASE')
+cursor = cnx.cursor()
+
+
+
 def Twitch(*args, **kwargs):
+	cnx
+	cursor = cnx.cursor()
+	query = ('SELECT user_row FROM db_table')
+	cursor.execute(query)
+	streams = cursor.fetchall()
 
 	# Bot settings
 	uname = 'TwitchBot'
 	icon = ':twitch:'
-	channel = '#ignorethis'
+	channel = '#main-chat'
 
 	#	Twitch.tv API URL
 	url = 'https://api.twitch.tv/kraken/streams/'
 
-	#Slack incoming webhook url
-	slack = 'Your incoming webhook URL'
-
-	# Streamers list
-	# IE. from the url http://www.twitch.tv/jekku0966 use jekku0966
-	streams = ['put', 'the', 'streamers', 'here']
+	#Slack related
+	slack = 'YOUR_SLACK_INCOMING_HOOK'
 
 	# Url list generated from base url
 	urls = []
@@ -31,83 +47,132 @@ def Twitch(*args, **kwargs):
 
 	# Generate API urls
 	for x in streams:
-		link = url+x
+		link = url+x[0]
 		urls.append(link)
+	cursor.close()
 
-	# Check if stream is up or down, if stream is up post is to slack
+	# Check the urls and parse the returned body content
 	for y in urls:
 		r = requests.get(y)
 		data = json.loads(r.text)
 
-		# Stream down, post url and stream down
+		# Stream down, print url and stream down to console, no need to post to Slack
 		if data[u'stream'] == None:
-			print "{} - Stream is down".format(y)
+			print '{} is currently offline'.format(y)
 		# If Twitch user name can be found in the named list post url and notify it's already posted
 		elif data[u'stream'][u'channel'][u'name'] in online:
-			print '{} is already posted'.format(y)
+			print data[u'stream'][u'channel'][u'name'] + ' is already posted'
 		else:
-			# Some users may have different GTs than they have on Twitch.tv this is the way to overcome it
-			if data[u'stream'][u'channel'][u'name'] == 'streamer':
-				requests.post(slack, json.dumps({'username': uname, 'icon_emoji': icon, 'channel': channel,
-                               'attachments': [{'fallback': 'Actual GT HERE' +' is live playing ' + data[u'stream'][u'game'] +'on <' + data[u'stream'][u'channel'][u'url'] + '|Twitch.tv>', 'title': data[u'stream'][u'channel'][u'name'] +' is live playing ' + data[u'stream'][u'game'],
+			# Post a nice notification to Slack main channel, with a preview image
+			if data[u'stream'][u'game'] == 'Battlefield Hardline':
+				requests.post(slack, json.dumps({'username': uname, 'icon_emoji': icon, 'channel': '#battlefield',
+                               'attachments': [{'fallback': data[u'stream'][u'channel'][u'name'] +' is live playing ' + data[u'stream'][u'game'] +'on <' + data[u'stream'][u'channel'][u'url'] + '|Twitch.tv>', 'title': data[u'stream'][u'channel'][u'name'] +' is live playing ' + data[u'stream'][u'game'],
                                  'title_link': data[u'stream'][u'channel'][u'url'],
-                                  'image_url': data[u'stream'][u'preview'][u'medium'],'color': '#7CD197'}]}))
+                                  'image_url': data[u'stream'][u'preview'][u'medium'],
+                                  'color': '#7CD197'}]}))
+				print data[u'stream'][u'channel'][u'name'] + ' posted to Slack'
+			elif data[u'stream'][u'game'] == 'Call of Duty: Advanced Warfare' or data[u'stream'][u'game'] == 'Call of Duty: Black Ops II':
+				requests.post(slack, json.dumps({'username': uname, 'icon_emoji': icon, 'channel': '#cod',
+                               'attachments': [{'fallback': data[u'stream'][u'channel'][u'name'] +' is live playing ' + data[u'stream'][u'game'] +'on <' + data[u'stream'][u'channel'][u'url'] + '|Twitch.tv>', 'title': data[u'stream'][u'channel'][u'name'] +' is live playing ' + data[u'stream'][u'game'],
+                                 'title_link': data[u'stream'][u'channel'][u'url'],
+                                  'image_url': data[u'stream'][u'preview'][u'medium'],
+                                  'color': '#7CD197'}]}))
+				print data[u'stream'][u'channel'][u'name'] + ' posted to Slack'
+			elif data[u'stream'][u'game'] == 'Minecraft: Xbox One Edition':
+				requests.post(slack, json.dumps({'username': uname, 'icon_emoji': icon, 'channel': '#minecraft',
+                               'attachments': [{'fallback': data[u'stream'][u'channel'][u'name'] +' is live playing ' + data[u'stream'][u'game'] +'on <' + data[u'stream'][u'channel'][u'url'] + '|Twitch.tv>', 'title': data[u'stream'][u'channel'][u'name'] +' is live playing ' + data[u'stream'][u'game'],
+                                 'title_link': data[u'stream'][u'channel'][u'url'],
+                                  'image_url': data[u'stream'][u'preview'][u'medium'],
+                                  'color': '#7CD197'}]}))
+				print data[u'stream'][u'channel'][u'name'] + ' posted to Slack'
+			elif data[u'stream'][u'game'] == 'Grand Theft Auto V':
+				requests.post(slack, json.dumps({'username': uname, 'icon_emoji': icon, 'channel': '#gtav',
+                               'attachments': [{'fallback': data[u'stream'][u'channel'][u'name'] +' is live playing ' + data[u'stream'][u'game'] +'on <' + data[u'stream'][u'channel'][u'url'] + '|Twitch.tv>', 'title': data[u'stream'][u'channel'][u'name'] +' is live playing ' + data[u'stream'][u'game'],
+                                 'title_link': data[u'stream'][u'channel'][u'url'],
+                                  'image_url': data[u'stream'][u'preview'][u'medium'],
+                                  'color': '#7CD197'}]}))
+				print data[u'stream'][u'channel'][u'name'] + ' posted to Slack'
+			elif data[u'stream'][u'game'] == 'Destiny':
+				requests.post(slack, json.dumps({'username': uname, 'icon_emoji': icon, 'channel': '#destiny',
+                               'attachments': [{'fallback': data[u'stream'][u'channel'][u'name'] +' is live playing ' + data[u'stream'][u'game'] +'on <' + data[u'stream'][u'channel'][u'url'] + '|Twitch.tv>', 'title': data[u'stream'][u'channel'][u'name'] +' is live playing ' + data[u'stream'][u'game'],
+                                 'title_link': data[u'stream'][u'channel'][u'url'],
+                                  'image_url': data[u'stream'][u'preview'][u'medium'],
+                                  'color': '#7CD197'}]}))
+				print data[u'stream'][u'channel'][u'name'] + 'posted to Slack'
+			elif data[u'stream'][u'game'] == 'Halo: The Master Chief Collection':
+				requests.post(slack, json.dumps({'username': uname, 'icon_emoji': icon, 'channel': '#halo_mcc',
+                               'attachments': [{'fallback': data[u'stream'][u'channel'][u'name'] +' is live playing ' + data[u'stream'][u'game'] +'on <' + data[u'stream'][u'channel'][u'url'] + '|Twitch.tv>', 'title': data[u'stream'][u'channel'][u'name'] +' is live playing ' + data[u'stream'][u'game'],
+                                 'title_link': data[u'stream'][u'channel'][u'url'],
+                                  'image_url': data[u'stream'][u'preview'][u'medium'],
+                                  'color': '#7CD197'}]}))
+				print data[u'stream'][u'channel'][u'name'] + ' posted to Slack'
 			else:
-				# Post a nice notification to Slack main channel, with a preview image
-				# If posting the attachment type message posting fails the script posts a normal message with the same info.
-				if data[u'stream'][u'game'] == 'Battlefield Hardline':
-					requests.post(slack, json.dumps({'username': uname, 'icon_emoji': icon, 'channel': '#battlefield',
+				requests.post(slack, json.dumps({'username': uname, 'icon_emoji': icon, 'channel': '#main-chat',
                                'attachments': [{'fallback': data[u'stream'][u'channel'][u'name'] +' is live playing ' + data[u'stream'][u'game'] +'on <' + data[u'stream'][u'channel'][u'url'] + '|Twitch.tv>', 'title': data[u'stream'][u'channel'][u'name'] +' is live playing ' + data[u'stream'][u'game'],
                                  'title_link': data[u'stream'][u'channel'][u'url'],
                                   'image_url': data[u'stream'][u'preview'][u'medium'],
                                   'color': '#7CD197'}]}))
-
-				elif data[u'stream'][u'game'] == 'Call of Duty: Advanced Warfare' or data[u'stream'][u'game'] == 'Call of Duty: Black Ops II':
-					requests.post(slack, json.dumps({'username': uname, 'icon_emoji': icon, 'channel': '#cod',
-                               'attachments': [{'fallback': data[u'stream'][u'channel'][u'name'] +' is live playing ' + data[u'stream'][u'game'] +'on <' + data[u'stream'][u'channel'][u'url'] + '|Twitch.tv>', 'title': data[u'stream'][u'channel'][u'name'] +' is live playing ' + data[u'stream'][u'game'],
-                                 'title_link': data[u'stream'][u'channel'][u'url'],
-                                  'image_url': data[u'stream'][u'preview'][u'medium'],
-                                  'color': '#7CD197'}]}))
-
-				elif data[u'stream'][u'game'] == 'Minecraft':
-					requests.post(slack, json.dumps({'username': uname, 'icon_emoji': icon, 'channel': '#minecraft',
-                               'attachments': [{'fallback': data[u'stream'][u'channel'][u'name'] +' is live playing ' + data[u'stream'][u'game'] +'on <' + data[u'stream'][u'channel'][u'url'] + '|Twitch.tv>', 'title': data[u'stream'][u'channel'][u'name'] +' is live playing ' + data[u'stream'][u'game'],
-                                 'title_link': data[u'stream'][u'channel'][u'url'],
-                                  'image_url': data[u'stream'][u'preview'][u'medium'],
-                                  'color': '#7CD197'}]}))
-
-				elif data[u'stream'][u'game'] == 'Grand Theft Auto V':
-					requests.post(slack, json.dumps({'username': uname, 'icon_emoji': icon, 'channel': '#gtav',
-                               'attachments': [{'fallback': data[u'stream'][u'channel'][u'name'] +' is live playing ' + data[u'stream'][u'game'] +'on <' + data[u'stream'][u'channel'][u'url'] + '|Twitch.tv>', 'title': data[u'stream'][u'channel'][u'name'] +' is live playing ' + data[u'stream'][u'game'],
-                                 'title_link': data[u'stream'][u'channel'][u'url'],
-                                  'image_url': data[u'stream'][u'preview'][u'medium'],
-                                  'color': '#7CD197'}]}))
-
-				elif data[u'stream'][u'game'] == 'Destiny':
-					requests.post(slack, json.dumps({'username': uname, 'icon_emoji': icon, 'channel': '#destiny',
-                               'attachments': [{'fallback': data[u'stream'][u'channel'][u'name'] +' is live playing ' + data[u'stream'][u'game'] +'on <' + data[u'stream'][u'channel'][u'url'] + '|Twitch.tv>', 'title': data[u'stream'][u'channel'][u'name'] +' is live playing ' + data[u'stream'][u'game'],
-                                 'title_link': data[u'stream'][u'channel'][u'url'],
-                                  'image_url': data[u'stream'][u'preview'][u'medium'],
-                                  'color': '#7CD197'}]}))
-
-				elif data[u'stream'][u'game'] == 'Halo: The Master Chief Collection':
-					requests.post(slack, json.dumps({'username': uname, 'icon_emoji': icon, 'channel': '#halo_mcc',
-                               'attachments': [{'fallback': data[u'stream'][u'channel'][u'name'] +' is live playing ' + data[u'stream'][u'game'] +'on <' + data[u'stream'][u'channel'][u'url'] + '|Twitch.tv>', 'title': data[u'stream'][u'channel'][u'name'] +' is live playing ' + data[u'stream'][u'game'],
-                                 'title_link': data[u'stream'][u'channel'][u'url'],
-                                  'image_url': data[u'stream'][u'preview'][u'medium'],
-                                  'color': '#7CD197'}]}))
-				else:
-					requests.post(slack, json.dumps({'username': uname, 'icon_emoji': icon, 'channel': '#ignorethis',
-                               'attachments': [{'fallback': data[u'stream'][u'channel'][u'name'] +' is live playing ' + data[u'stream'][u'game'] +'on <' + data[u'stream'][u'channel'][u'url'] + '|Twitch.tv>', 'title': data[u'stream'][u'channel'][u'name'] +' is live playing ' + data[u'stream'][u'game'],
-                                 'title_link': data[u'stream'][u'channel'][u'url'],
-                                  'image_url': data[u'stream'][u'preview'][u'medium'],
-                                  'color': '#7CD197'}]}))
+				print data[u'stream'][u'channel'][u'name'] + ' posted to Slack'
 			# Put Twitch username to a list for later inspection
 			online.append(data[u'stream'][u'channel'][u'name'])
 
+class server(object):
+	exposed = True
+
+	def GET(*args, **kwargs): # Print to console on GET and return a html page if the url is visited
+		print 'We got pinged'
+		return file('index.html')
+
+	def POST(*args, **kwargs):
+		cnx
+		cursor = cnx.cursor()
+		json_parse = json.dumps(cherrypy.request.body.params)
+		# Decode the json to trigger add/rem
+		decoded = json.loads(json_parse)
+		trigger = decoded['text']
+		keyword = trigger[8:11]
+
+		if keyword == 'add':
+			cnx
+			# Get the username from trigger after add
+			user = trigger[12:]
+			try:
+				add = ('INSERT INTO db_table(user_row) VALUES ("{}")').format(user.lower())
+				cursor.execute(add)
+			except Exception as e:
+				print e
+				cnx.rollback()
+			cursor.close()
+			return json.dumps({'text': user + ' added to streamer database.'})
+			print user + ' added to streamer database.'
+		elif keyword == 'rem':
+			cnx
+			# Get the username from trigger after rem
+			user = trigger[12:]
+			try:
+				remove = ('DELETE FROM db_table WHERE user_row = "{}"').format(user.lower())
+				cursor.execute(remove)
+			except Exception as e:
+				print e
+				cnx.rollback()
+			cursor.close()
+			return json.dumps({'text': user + ' removed from streamer database.'})
+			print user + ' removed from streamer database.'
+		else:
+			# This gets returned to Slack and a notification is printed to console
+			print trigger + ' (Wrong keyword used)'
+			return json.dumps({'text': 'Please check your keyword. I understand only "add" or "rem".\nSo a working command is "!twitch add username" where the username is the one in the actual Twitch.tv url.'})
+
+
+def Config(*args, **kwargs): # CherryPy server conf files
+	cherrypy.config.update("server.conf")
+	cherrypy.quickstart(server(), '/', "app.conf")
+	scheduler.start()
+
 		
 if __name__ == '__main__': # APScheduler start
-	scheduler = BlockingScheduler()
-	scheduler.add_job(Twitch, 'interval', seconds=30)
+	scheduler = BackgroundScheduler(job_defaults=job_defaults)
+	scheduler.add_job(Twitch, 'interval', seconds=120)
 	print "Bot started!"
 	scheduler.start()
+	Config()
