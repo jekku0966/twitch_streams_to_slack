112883ed4da36736098a6163f1e5e6c7cedba17c
jekku0966 <Jerva@Jerva-iMac.local>
2015-03-25 23:24:53 +0200

    no message

--- stream_status.py
+++ stream_status.py
@@ -9,21 +9,20 @@ job_defaults = {
     'max_instances': 3
 }
 
-
 # Posted urls
 online = []
 
 # MySQL database credentials
-cnx = MySQLdb.connect(user='USERNAME', passwd='PASSWORD',
-                              host='DATABASE_IP',
-                              db='DATABASE_NAME')
+usern = 'USERNAME'
+ppwd = 'PASSWORD'
+host = 'DATABASE_IP'
+dbase = 'DATABASE'
+cnx = MySQLdb.connect(user=usern, passwd=ppwd, host=host, db=dbase)
 
 cursor = cnx.cursor()
 
 
-def Twitch(*args, **kwargs):
-	cnx
-	cursor = cnx.cursor()
+def Twitch():
 	query = ('SELECT table_row FROM db_table')
 	cursor.execute(query)
 	streams = cursor.fetchall()
@@ -34,12 +33,12 @@ def Twitch(*args, **kwargs):
 	channel = '#main-chat'
 
 	#	Twitch.tv API URL
-	url = 'https://api.twitch.tv/kraken/streams/'
-
-	# Channel dict, 'NAME OF THE GAME ON TWITCH': '#CHANNEL'
+	api_url = 'https://api.twitch.tv/kraken/streams/'
+	
+	# Channel dict
 	games = {'Battlefield Hardline': '#battlefield', 'Call of Duty: Advanced Warfare': '#cod', 'Call of Duty: Black Ops II': '#cod', 'Minecraft: Xbox One Edition': '#minecraft', 'Grand Theft Auto V': '#gtav', 'Destiny': '#destiny', 'Halo: The Master Chief Collection': '#halo_mcc'}
 
-	#Slack incoming webhook
+	#Slack related
 	slack = 'SLACK_INCOMING_WEBHOOK'
 
 	# Url list generated from base url
@@ -49,29 +48,28 @@ def Twitch(*args, **kwargs):
 	print 'Running the list'
 
 	# Generate API urls
-	for x in streams:
-		link = url+x[0]
+	for user_name in streams:
+		link = api_url+user_name[0]
 		urls.append(link)
 
 	# Check if stream is up or down, if stream is up post is to slack
-	for y in urls:
-		r = requests.get(y)
+	for full_url in urls:
+		r = requests.get(full_url)
 		data = json.loads(r.text)
 
-		# Stream down, print url and stream down to console
+		# Stream down, post url and stream down
 		if data[u'stream'] == None:
-			print '{} is currently offline'.format(y)
-		# If Twitch user name can be found in online list print it out to console
+			print '{} is currently offline'.format(full_url)
+		# If Twitch user name can be found in the named list post url and notify it's already posted
 		elif data[u'stream'][u'channel'][u'name'] in online:
 			print data[u'stream'][u'channel'][u'name'] + ' is already posted'
 		else:
-				# Post a nice notification to appropriate channel with a preview image
+				# Post a nice notification to Slack main channel, with a preview image
 			if data[u'stream'][u'game'] in games:
-				requests.post(slack, json.dumps({'username': uname, 'icon_emoji': icon, 'channel': games[data[u'stream'][u'game']],'attachments': [{'fallback': data[u'stream'][u'channel'][u'name'] +' is live playing ' + data[u'stream'][u'game'] +'on <' + data[u'stream'][u'channel'][u'url'] + '|Twitch.tv>', 'title': data[u'stream'][u'channel'][u'name'] +' is live playing ' + data[u'stream'][u'game'],'title_link': data[u'stream'][u'channel'][u'url'],'image_url': data[u'stream'][u'preview'][u'medium'],'color': '#7CD197'}]}))
+				requests.post(slack, json.dumps({'username': uname, 'icon_emoji': icon, 'channel': games[data[u'stream'][u'game']], 'attachments': [{'fallback': data[u'stream'][u'channel'][u'name'] +' is live playing ' + data[u'stream'][u'game'] +'on <' + data[u'stream'][u'channel'][u'url'] + '|Twitch.tv>', 'title': data[u'stream'][u'channel'][u'name'] +' is live playing ' + data[u'stream'][u'game'], 'title_link': data[u'stream'][u'channel'][u'url'], 'image_url': data[u'stream'][u'preview'][u'medium'], 'color': '#7CD197'}]}))
 				print data[u'stream'][u'channel'][u'name'] + ' posted to' + games[data[u'stream'][u'game']] + ' in Slack'
 			else:
-				# If channel not defined post to main chat (Remember to change the channel key)
-				requests.post(slack, json.dumps({'username': uname, 'icon_emoji': icon, 'channel': '#main-chat','attachments': [{'fallback': data[u'stream'][u'channel'][u'name'] +' is live playing ' + data[u'stream'][u'game'] +' on <' + data[u'stream'][u'channel'][u'url'] + '|Twitch.tv>', 'title': data[u'stream'][u'channel'][u'name'] +' is live playing ' + data[u'stream'][u'game'],'title_link': data[u'stream'][u'channel'][u'url'],'image_url': data[u'stream'][u'preview'][u'medium'],'color': '#7CD197'}]}))
+				requests.post(slack, json.dumps({'username': uname, 'icon_emoji': icon, 'channel': '#main-chat', 'attachments': [{'fallback': data[u'stream'][u'channel'][u'name'] +' is live playing ' + data[u'stream'][u'game'] +' on <' + data[u'stream'][u'channel'][u'url'] + '|Twitch.tv>', 'title': data[u'stream'][u'channel'][u'name'] +' is live playing ' + data[u'stream'][u'game'], 'title_link': data[u'stream'][u'channel'][u'url'], 'image_url': data[u'stream'][u'preview'][u'medium'], 'color': '#7CD197'}]}))
 				print data[u'stream'][u'channel'][u'name'] + ' posted to #main-chat in Slack'
 			# Put Twitch username to a list for later inspection
 			online.append(data[u'stream'][u'channel'][u'name'])
@@ -79,54 +77,42 @@ def Twitch(*args, **kwargs):
 class server(object):
 	exposed = True
 
-	def GET(*args, **kwargs): # Print to console on GET and return a html page if the url is visited
+	def GET(): # Print to console on GET and return a html page if the url is visited
 		print 'We got pinged'
 		return file('index.html')
 
-	def POST(*args, **kwargs):
+	def POST():
 		json_parse = json.dumps(cherrypy.request.body.params)
 		# Decode the json
 		decoded = json.loads(json_parse)
 		trigger = decoded['text']
 		keyword = trigger[8:11]
 
-		if keyword == 'add': # This catches 'add' and add the username to the database
-			cnx
-			cursor = cnx.cursor()
+		if keyword == 'add':
 			user = trigger[12:]
-			try:
-				add = ('INSERT INTO db_table(table_row) VALUES ("{}")').format(user.lower())
-				cursor.execute(add)
-			except Exception as e:
-				print e
-				cnx.rollback()
+			add = ('INSERT INTO db_table(table_row) VALUES ("{}")').format(user.lower())
+			cursor.execute(add)
 			return json.dumps({'text': user + ' added to streamer database.'})
 			print user + ' added to streamer database.'
-		elif keyword == 'rem': # This catches 'rem' and deletes a username from the database
-			cnx
-			cursor = cnx.cursor()
+		elif keyword == 'rem':
 			user = trigger[12:]
-			try:
-				remove = ('DELETE FROM db_table WHERE table_row = "{}"').format(user.lower())
-				cursor.execute(remove)
-			except Exception as e:
-				print e
-				cnx.rollback()
+			remove = ('DELETE FROM db_table WHERE table_row = "{}"').format(user.lower())
+			cursor.execute(remove)
 			return json.dumps({'text': user + ' removed from streamer database.'})
 			print user + ' removed from streamer database.'
-		else: # This returns a wrong keyword used message
+		else:
 			print trigger + ' (Wrong keyword used)'
 			return json.dumps({'text': 'Please check your keyword. I understand only "add" or "rem".\nSo a working command is "!twitch add username" where the username is the one in the actual Twitch.tv url.'})
 
 
-def Config(*args, **kwargs): # CherryPy server conf files
+def Config(): # CherryPy server conf files
 	cherrypy.config.update("server.conf")
 	cherrypy.quickstart(server(), '/', "app.conf")
 	scheduler.start()
 
 		
 if __name__ == '__main__': # APScheduler start
-	scheduler = BackgroundScheduler()
+	scheduler = BackgroundScheduler(job_defaults=job_defaults)
 	scheduler.add_job(Twitch, 'interval', seconds=120)
 	print "Bot started!"
 	scheduler.start()
